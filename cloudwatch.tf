2487b51ed435b9d211b212a1d770b7b8d2978740
Andres Solarte <devsolarte@gmail.com>
2023-08-18 10:30:21 -0500

    feat: Cloudwatch logs for VPN connection tunnels

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,8 +1,12 @@
-resource "aws_cloudwatch_log_group" "vpn_logs" {
-  name              = "/aws/vpc/vpn-connections/${local.vpn_connection_name}"
+locals {
+  vpn_aws_cloudwatch_log_group_name = "/aws/vpc/vpn-connections/${local.vpn_connection_name}"
+}
+
+resource "aws_cloudwatch_log_group" "vpn" {
+  name              = local.vpn_aws_cloudwatch_log_group_name
   retention_in_days = var.cloudwatch_logs_retention_in_days
 
   tags = {
-    Name = "/aws/vpc/vpn-connections/${local.vpn_connection_name}"
+    Name = local.vpn_aws_cloudwatch_log_group_name
   }
 }
