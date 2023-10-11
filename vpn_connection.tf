c4db61758677df0a42ddf8e37bb054ea96d5b44b
Andres Solarte <devsolarte@gmail.com>
2023-10-10 19:48:31 -0500

    feat: re enabled local_ipv4_network_cidr and remote_ipv4_network_cidr in aws_vpn_connection

--- vpn_connection.tf
+++ vpn_connection.tf
@@ -8,8 +8,8 @@ resource "aws_vpn_connection" "main" {
   type                     = aws_customer_gateway.vpn.type
   static_routes_only       = true
   tunnel_inside_ip_version = "ipv4"
-  local_ipv4_network_cidr  = "0.0.0.0/0" # var.remote_vpc_cidr
-  remote_ipv4_network_cidr = "0.0.0.0/0" # data.aws_vpc.local_vpc.cidr_block
+  local_ipv4_network_cidr  = var.remote_vpc_cidr
+  remote_ipv4_network_cidr = data.aws_vpc.local_vpc.cidr_block
 
   tunnel1_preshared_key = replace(join("-", [var.environment, local.solution_name, "vpn-conn-t1-psk"]), "-", "_")
   tunnel1_ike_versions  = ["ikev2"]
